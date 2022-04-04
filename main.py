5d346b45bbfdc07b1474fa6caf12b0e36aa5d85a
Charles Lee Lin Ta <e0484321@u.nus.edu>
2022-04-04 11:46:46 +0800

    .

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from bs4.element import Tag
 import codecs
 import re
 import nltk
-# import pycrfsuite
-# import numpy as np
-# from sklearn.metrics import classification_report
+import pycrfsuite
+import numpy as np
+from sklearn.metrics import classification_report
 
 
 from contractions import CONTRACTION_MAP
 from nltk.stem import WordNetLemmatizer
-# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 # nltk.download('punkt')
 
@@ -106,98 +106,96 @@ for i, doc in enumerate(docs):
     # Take the word, POS tag, and its label
     data.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
 
-print(data[0])
+def word2features(sentence, i):
+    current_word = sentence[i][0]
+    current_pos = sentence[i][1]
 
-# def word2features(sentence, i):
-#     current_word = sentence[i][0]
-#     current_pos = sentence[i][1]
-#
-#     lemmatizer = WordNetLemmatizer()
-#     superlatives = ['JJS', 'RBS']
-#     comparatives = ['JJR', 'RBR']
-#
-#     features = [
-#         'bias',
-#         'word.lower=' + current_word.lower(),
-#         'word[-3:]=' + current_word[-3:],
-#         'word[-2:]=' + current_word[-2:],
-#         'postag=' + current_pos,
-#     ]
-#
-#     # Features for words that are not at the beginning of a document
-#     if i > 0:
-#         prev_word = sentence[i-1][0]
-#         previous_pos = sentence[i-1][1]
-#         features.extend([
-#             '-1:word.lower=' + prev_word.lower(),
-#             '-1:postag=' + previous_pos,
-#         ])
-#     else:
-#         features.append('BOS')
-#
-#     # Features for words that are not at the end of a document
-#     if i < len(sentence) - 1:
-#         next_word = sentence[i + 1][0]
-#         next_pos = sentence[i + 1][1]
-#         features.extend([
-#             '+1:word.lower=' + next_word.lower(),
-#             '+1:postag=' + next_pos,
-#         ])
-#     else:
-#         features.append('EOS')
-#
-#     return features
-#
-# def extract_features(sentence):
-#     return [word2features(sentence, i) for i in range(len(sentence))]
-#
-# def get_label(sentence):
-#     return [label for (token, pos, label) in sentence]
+    lemmatizer = WordNetLemmatizer()
+    superlatives = ['JJS', 'RBS']
+    comparatives = ['JJR', 'RBR']
 
-# def train_and_validate_model():
-#
-#     X = [extract_features(sentence) for sentence in data]
-#     y = [get_label(sentence) for sentence in data]
-#
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#
-#     trainer = pycrfsuite.Trainer(verbose=False)
-#     for xseq, yseq in zip(X_train, y_train):
-#         trainer.append(xseq, yseq)
-#
-#     trainer.set_params({
-#         # coefficient for L1 penalty
-#         'c1': 0.1,
-#
-#         # coefficient for L2 penalty
-#         'c2': 0.01,
-#
-#         # maximum number of iterations
-#         'max_iterations': 200,
-#
-#         # whether to include transitions that
-#         # are possible, but not observed
-#         'feature.possible_transitions': True
-#     })
-#
-#     trainer.train('crf.model')
-#
-#
-#     tagger = pycrfsuite.Tagger()
-#     tagger.open('crf.model')
-#
-#     y_pred = [tagger.tag(xseq) for xseq in X_test]
-#
-#     labels = {"B": 0, 'I': 1, 'O': 2} # row indexes for position of labels in the classification matrix
-#
-#     predictions = np.array([labels[tag] for row in y_pred for tag in row])
-#     truths = np.array([labels[tag] for row in y_test for tag in row])
-#
-#     print(classification_report(truths, predictions, target_names=['B', 'I', 'O']))
-#
-#     pass
+    features = [
+        'bias',
+        'word.lower=' + current_word.lower(),
+        'word[-3:]=' + current_word[-3:],
+        'word[-2:]=' + current_word[-2:],
+        'postag=' + current_pos,
+    ]
+
+    # Features for words that are not at the beginning of a document
+    if i > 0:
+        prev_word = sentence[i-1][0]
+        previous_pos = sentence[i-1][1]
+        features.extend([
+            '-1:word.lower=' + prev_word.lower(),
+            '-1:postag=' + previous_pos,
+        ])
+    else:
+        features.append('BOS')
+
+    # Features for words that are not at the end of a document
+    if i < len(sentence) - 1:
+        next_word = sentence[i + 1][0]
+        next_pos = sentence[i + 1][1]
+        features.extend([
+            '+1:word.lower=' + next_word.lower(),
+            '+1:postag=' + next_pos,
+        ])
+    else:
+        features.append('EOS')
+
+    return features
+
+def extract_features(sentence):
+    return [word2features(sentence, i) for i in range(len(sentence))]
+
+def get_label(sentence):
+    return [label for (token, pos, label) in sentence]
+
+def train_and_validate_model():
+
+    X = [extract_features(sentence) for sentence in data]
+    y = [get_label(sentence) for sentence in data]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    trainer = pycrfsuite.Trainer(verbose=False)
+    for xseq, yseq in zip(X_train, y_train):
+        trainer.append(xseq, yseq)
+
+    trainer.set_params({
+        # coefficient for L1 penalty
+        'c1': 0.1,
+
+        # coefficient for L2 penalty
+        'c2': 0.01,
+
+        # maximum number of iterations
+        'max_iterations': 200,
+
+        # whether to include transitions that
+        # are possible, but not observed
+        'feature.possible_transitions': True
+    })
+
+    trainer.train('crf.model')
+
+
+    tagger = pycrfsuite.Tagger()
+    tagger.open('crf.model')
+
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
+
+    labels = {"B": 0, 'I': 1, 'O': 2} # row indexes for position of labels in the classification matrix
+
+    predictions = np.array([labels[tag] for row in y_pred for tag in row])
+    truths = np.array([labels[tag] for row in y_test for tag in row])
+
+    print(classification_report(truths, predictions, target_names=['B', 'I', 'O']))
+
+    pass
 
 
 
-# if __name__ == "__main__":
-#     train_and_validate_model()
+if __name__ == "__main__":
+    train_and_validate_model()
