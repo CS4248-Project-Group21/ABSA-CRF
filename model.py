04476cc4c7efa4f380d581bd074089a8b9c684e9
xnajasho <68313625+xnajasho@users.noreply.github.com>
2022-04-04 17:30:49 +0800

    added new metric feature

--- model.py
+++ model.py
@@ -210,6 +210,61 @@ class CNFModel:
         truths = np.array([labels[tag] for row in y_test for tag in row])
         print(classification_report(truths, predictions, target_names=['B', 'I', 'O']))
 
+        new_y_test = list(map(lambda x: list(map(self.change_BIO, x)), y_test))
+        new_y_pred = list(map(lambda x: list(map(self.change_BIO, x)), y_pred))
+
+        print(self.get_metrics(new_y_test, new_y_pred, b=1))  ## printing new metric to calculate F1
+
+
+    '''
+        Helper Function to Change Bio label to numerical values. "O" = 0, "B" = 1, "I" = 2
+    '''
+    def change_BIO(self, label):
+        if label == 'O':
+            return 0
+        elif label == 'B':
+            return 1
+        else:
+            return 2
+
+    '''
+        Helper Function for get_metrics() to calculate new F1 metric measure
+    '''
+    def get_term_pos(self, labels):
+        start, end = 0, 0
+        tag_on = False
+        terms = []
+        labels = np.append(labels, [0])
+        for i, label in enumerate(labels):
+            if label == 1 and not tag_on:
+                tag_on = True
+                start = i
+            if tag_on and labels[i + 1] != 2:
+                tag_on = False
+                end = i
+                terms.append((start, end))
+        return terms
+
+    '''
+        Function to calculate new metric to evaluate our model instead of classification report.
+    '''
+    def get_metrics(self, test_y, pred_y, b=1):
+        common, relevant, retrieved = 0., 0., 0.
+        for i in range(len(test_y)):
+            cor = self.get_term_pos(test_y[i])
+            pre = self.get_term_pos(pred_y[i])
+            common += len([a for a in pre if a in cor])
+            retrieved += len(pre)
+            relevant += len(cor)
+        p = common / retrieved if retrieved > 0 else 0.
+        r = common / relevant
+        f1 = (1 + (b ** 2)) * p * r / ((p * b ** 2) + r) if p > 0 and r > 0 else 0.
+
+        text = "Precision = {pre}, Recall = {rec}, F1 = {f1_score}," \
+               " Common = {com}, Retrieved = {ret}, Relevant = {rel}".format(pre=p, rec=r, f1_score=f1, com=common,
+                                                                             ret=retrieved, rel=relevant)
+        return text
+
 
 if __name__ == "__main__":
     model = CNFModel()
